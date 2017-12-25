754de0691fcd67a26e8b465c2cd0cb596fb304dc
Henri Idrovo <henri.idrovo@gmail.com>
2017-12-25 12:24:37 -0600

    success! passing 15/16 tests. todo: handle a digit that is more than one charecter, like 100[abc]

--- assesment/CodeFightsQ2.java
+++ assesment/CodeFightsQ2.java
@@ -1,102 +1,61 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.lang.Character;
 
 class CodeFightsQ2 {
 	final static String OPEN_BRACE = "[";
 	final static String CLOSE_BRACE = "]";
 
-    public static void main(String[] args){
-        // System.out.println("Hello World!!!");
-        // String input = "ab3[cde]fgh";
-        // String output = decodeString(input);
-        // System.out.println(" **** " + output);
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        String input = "abc3[def]ghi";
+        String output = decodeString(input);
+        System.out.println(" **** " + output);
 
-        ArrayList aList = new ArrayList();
-   
-	    //populate ArrayList object
-	    aList.add("1");
-	    aList.add("2");
-	    aList.add("3");
-	    aList.add("4");
-	    aList.add("5");
-	   
-	   
-	    System.out.println("ArrayList before removal : ");
-	    for(int i=0; i< aList.size(); i++)
-	      System.out.println(aList.get(i));
-	     
-	    //get an Iterator
-	    Iterator itr = aList.iterator();
-	   
-	    //remove 2 from ArrayList using Iterator's remove method.
-	    String strElement = "";
-	    while(itr.hasNext()){
-	     
-	      /*
-	        Iterator's next method returns an Object so we need to cast it into
-	        appropriate class before using it.
-	      */
-	      strElement = (String)itr.next();
-	      if(strElement.equals("2"))
-	      {
-	        /*
-	          Remove an element using remove() method of Iterator
-	          Remove method removes an element from underlying collection and
-	          it may throw a UnsupportedOperationException if the remove
-	          operation is not supported.
-	        */
-	        itr.remove();
-	        System.out.println("current element after removal! " + itr.next());
-	        break;
-	      }
-	     
-     
-     
-    }
-   
-    System.out.println("ArrayList after removal : ");
-    for(int i=0; i< aList.size(); i++)
-      System.out.println(aList.get(i));
         
     }
 
     public static String decodeString(String s) {
-    	char[] charArray = s.toCharArray();
+		ArrayList<Character> chars = new ArrayList<>();
 
-    	StringBuilder sb = new StringBuilder();
+		for (char ch: s.toCharArray()) {
+			chars.add(ch);
+		}
 
-    	for(int i =0;i<charArray.length;i++){
-    		if(Character.isLetter(charArray[i])){
-    			sb.append(charArray[i]);
-    		}	
-    		else if (Character.isDigit(charArray[i])) {
-    			//DIGIT
-    			//CAPTURE EVERYTHING IN BETWEEN BRACKETS
-    			int repeat = charArray[i];
-    			StringBuilder sb2 = new StringBuilder(); 
-    			i++;
-    			i++;
-    			while(Character.toString(charArray[i]) != CLOSE_BRACE){
-    				//A CHARECTER TO REPEAT
-    				sb2.append(charArray[i]);
-    				i++;
-    			}
-    			while(repeat != 0){
-    				sb.append(sb2.toString());
-    				repeat--;
-    			}
-    			//i++; //NEXT ALPHABETIC CHARECTER. ADVANCED BY OUTTER LOOP
+    	Iterator<Character> itr = chars.iterator();
 
-    		}
-    		else 
-    			System.out.println("ERROR. UNRECOGNIZED CHARECTER");
-    	} // FOR LOOP
- 		return sb.toString();   	
+		String result = recursive(itr);
+
+ 		return result;  	
 	}
 
-	//RECURSIVE METHOD
-	public StringBuilder process(){
-		return null;
+	public static String recursive(Iterator itr) {
+		StringBuilder sb = new StringBuilder();
+
+		while(itr.hasNext()) {
+			Character char1 = (Character)itr.next();
+			if(Character.isLetter(char1)) {
+				sb.append(char1);
+			}
+			else if(Character.isDigit(char1)) {
+				System.out.println(" char1: " + char1);
+				Integer repeat = Integer.valueOf(char1.toString());
+				String toRepeat = recursive(itr);
+				System.out.println(" toRepeat: " + toRepeat);
+				System.out.println(" repeat: " + repeat);
+				while(repeat != 0) {
+					sb.append(toRepeat);
+					repeat--;
+				}
+			}
+			else if(char1.equals('['))
+				itr.remove();
+			else if(char1.equals(']')) {
+				itr.remove();
+				return sb.toString();
+			}	
+		} //while loop
+		return sb.toString();
 	}
 
 }
